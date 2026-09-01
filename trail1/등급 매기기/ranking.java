90b8178c705518dd6a16fb1ec362b376938f644e
WOOTAK YOON <yoonwootak@naver.com>
2026-09-01 17:17:11 +0900

    [Passed] 등급 매기기 (93ms, 9MB)

---
+++ trail1/등급 매기기/ranking.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        double a = Integer.parseInt(br.readLine());
+
+        if (a >= 90) {
+            System.out.println("A");
+        }
+        else if (a >= 80) {
+            System.out.println("B");
+        }
+        else if (a >= 70) {
+            System.out.println("C");
+        }
+        else if (a >= 60) {
+            System.out.println("D");
+        }
+        else {
+            System.out.println("F");
+        }
+    }
+}
