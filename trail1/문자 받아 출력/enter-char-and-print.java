929c02ecc6e20787ce047b7812f812bc638955cb
WOOTAK YOON <yoonwootak@naver.com>
2026-09-01 16:19:24 +0900

    [Passed] 문자 받아 출력 (93ms, 9MB)

---
+++ trail1/문자 받아 출력/enter-char-and-print.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        char c = br.readLine().charAt(0);
+
+        System.out.println(c);
+    }
+}
