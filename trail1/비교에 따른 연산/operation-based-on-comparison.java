e11a6815f2fa4832788f73fa5ea07d87485d7a5f
WOOTAK YOON <yoonwootak@naver.com>
2026-09-01 17:08:29 +0900

    [Passed] 비교에 따른 연산 (90ms, 9MB)

---
+++ trail1/비교에 따른 연산/operation-based-on-comparison.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        if (a > b) {
+            System.out.println(a*b);
+        }
+        else {
+            System.out.println(b/a);
+        }
+    }
+}
