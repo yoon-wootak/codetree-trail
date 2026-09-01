cf662e508b458713cf811fb0a3d2c47bc9aa4e23
WOOTAK YOON <yoonwootak@naver.com>
2026-09-01 16:28:22 +0900

    [Passed] 날짜 변경하여 출력 2 (92ms, 9MB)

---
+++ trail1/날짜 변경하여 출력 2/print-date-with-different-format-2.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine();
+        String[] arr = s.split("-");
+
+        System.out.println(arr[2] + "." + arr[0] + "." + arr[1]);
+    }
+}
