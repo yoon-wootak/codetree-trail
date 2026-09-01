335a5f173777a1b0444246795a38d7b08a6e0a76
WOOTAK YOON <yoonwootak@naver.com>
2026-09-01 16:57:27 +0900

    [Passed] 합과 평균의 차 (105ms, 9MB)

---
+++ trail1/합과 평균의 차/sub-of-average-and-sum.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        System.out.printf("%d\n%d\n%d", a+b+c, (a+b+c)/3, a+b+c-(a+b+c)/3);
+    }
+}
