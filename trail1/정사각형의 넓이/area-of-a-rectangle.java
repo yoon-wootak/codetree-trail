427a83c9ebef394439d7c0c5bbba972e60b78662
WOOTAK YOON <yoonwootak@naver.com>
2026-09-01 17:04:21 +0900

    [Passed] 정사각형의 넓이 (93ms, 9MB)

---
+++ trail1/정사각형의 넓이/area-of-a-rectangle.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        System.out.println(N*N);
+        if (N < 5) {
+            System.out.println("tiny");
+        }
+    }
+}
