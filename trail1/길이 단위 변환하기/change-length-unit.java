c58784d290a7059ee6a3558c78d343801c43b140
WOOTAK YOON <yoonwootak@naver.com>
2026-09-01 15:21:30 +0900

    [Passed] 길이 단위 변환하기 (107ms, 9MB)

---
+++ trail1/길이 단위 변환하기/change-length-unit.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("%.1fft = %.1fcm\n", 9.2, 9.2*30.48);
+        System.out.printf("%.1fmi = %.1fcm", 1.3, 1.3*160934);
+    }
+}
