3dbcb2813880a7f372831e688d15e41a55349bec
WOOTAK YOON <yoonwootak@naver.com>
2026-09-01 15:59:57 +0900

    [Passed] 입력받은 값 교체하기 (104ms, 12MB)

---
+++ trail1/입력받은 값 교체하기/changing-inputs.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int temp;
+
+        temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println(a + " " + b);       
+    }
+}
