d1c872331505bf03af87be3bfd5f3f8604759ccf
WOOTAK YOON <yoonwootak@naver.com>
2026-09-01 15:38:42 +0900

    [Passed] 변수 값 교체하기 6 (90ms, 9MB)

---
+++ trail1/변수 값 교체하기 6/replacing-variable-values-6.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 5;
+        int temp = 0;
+
+        temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println(a + "\n" + b);
+    }
+}
