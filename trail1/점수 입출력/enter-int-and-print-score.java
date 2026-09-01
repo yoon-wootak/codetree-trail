581e89def837de233d952b10a4144e997a702318
WOOTAK YOON <yoonwootak@naver.com>
2026-09-01 15:51:56 +0900

    [Passed] 점수 입출력 (89ms, 9MB)

---
+++ trail1/점수 입출력/enter-int-and-print-score.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        System.out.println("Your score is " + N + " point.");
+    }
+}
