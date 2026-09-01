5c86e445947f3daedfb72865f5ef20802c0cdd22
WOOTAK YOON <yoonwootak@naver.com>
2026-09-01 16:15:38 +0900

    [Passed] 실수 입력받아 계산 (104ms, 9MB)

---
+++ trail1/실수 입력받아 계산/calculation-by-inputting-a-real-number.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        double a = Double.parseDouble(br.readLine());
+        double b = Double.parseDouble(br.readLine());
+
+        System.out.printf("%.2f", a+b);
+    }
+}
