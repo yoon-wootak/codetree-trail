e88eea16475e4d98727098ddbdc698d8bee53848
WOOTAK YOON <yoonwootak@naver.com>
2026-09-01 15:45:20 +0900

    [Passed] 데이터 교환 (89ms, 9MB)

---
+++ trail1/데이터 교환/exchange-data.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int A = 5;
+        int B = 6;
+        int C = 7;
+
+        int a, b, c;
+
+        b = A;
+        c = B;
+        a = C;
+
+        System.out.println(a + "\n" + b + "\n" + c);
+    }
+}
