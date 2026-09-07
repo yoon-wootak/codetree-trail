b2a447203cb400a6c8a28169377de44fed31207f
WOOTAK YOON <yoonwootak@naver.com>
2026-09-07 23:22:38 +0900

    [Passed] 괄호 쌍 만들어주기 3 (124ms, 10MB)

---
+++ trail2/괄호 쌍 만들어주기 3/pair-parentheses-3.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        
+        int count = 0;
+        for (int i=0; i<str.length(); i++) {
+            if (str.charAt(i) == '(') {
+                for (int j=i+1; j<str.length(); j++) {
+                    if (str.charAt(j) == ')') count++;
+                }
+            }
+        }
+
+        System.out.println(count);
+    }
+}
