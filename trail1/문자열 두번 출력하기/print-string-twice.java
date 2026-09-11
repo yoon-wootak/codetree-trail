15f9d967bd10c522f5e22dab4581b2c2bf9ecc67
WOOTAK YOON <yoonwootak@naver.com>
2026-09-11 17:26:11 +0900

    [Passed] 문자열 두번 출력하기 (93ms, 9MB)

---
+++ trail1/문자열 두번 출력하기/print-string-twice.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+
+        System.out.println(s);
+        System.out.println(s);
+    }
+}
